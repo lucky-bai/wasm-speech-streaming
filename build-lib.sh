444bb79f35d25b8c3e95d84bfacb706ce2b7f4b9
Bai Li <bai.li.2005@gmail.com>
2025-08-24 15:36:39 -0700

    update tokenizer url

--- build-lib.sh
+++ build-lib.sh
@@ -8,5 +8,5 @@ fi
 
 rm -rf build
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/wasm_speech_streaming.wasm --out-dir build --target web --split-linked-modules
+wasm-bindgen target/wasm32-unknown-unknown/release/wasm_speech_streaming.wasm --out-dir build --target web
 python server.py
