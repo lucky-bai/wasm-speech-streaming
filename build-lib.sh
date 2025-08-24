c9e9ba70e49ee8e02ce56835d473531fafa6fbf1
Bai Li <bai.li.2005@gmail.com>
2025-08-24 15:24:31 -0700

    use server.py

--- build-lib.sh
+++ build-lib.sh
@@ -1,5 +1,12 @@
 set -e
+
+if [ ! -f css/tailwind-3.4.17.js ]; then
+    echo "Downloading tailwind-3.4.17.js..."
+    mkdir -p css
+    curl -L -o css/tailwind-3.4.17.js https://cdn.tailwindcss.com/3.4.17
+fi
+
 rm -rf build
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/release/wasm_speech_streaming.wasm --out-dir build --target web --split-linked-modules
-python -m http.server
+python server.py
